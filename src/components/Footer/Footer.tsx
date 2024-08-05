c4117a17ac2fa3b7a64f4b741b7f429fcc47da29
shamision <shami0sion@gmail.com>
2024-08-05 21:35:01 +0200

    added the logo to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,6 +8,9 @@ const Footer = () => {
             <div className="flex flex-col justify-center items-center">
                 <div className="flex justify-center items-center  border border-[#8fc740] w-[1200px] h-[1200px] mt-20 rounded-full">
                     <div className="border border-[#8fc740] w-[1000px] h-[1000px] rounded-full text-white flex flex-col alig justify-center items-center font-thin">
+                        <div className="w-[240px] mx-auto h-[100px] relative overflow-hidden">
+                            <Image src={"/Logo.png"} fill alt="Logo" />
+                        </div>
                         <p>Kigali - Rwanda</p>
                         <p>KN .55 KG 676 st</p>
                         <p>Kimihurura, Gasabo</p>
